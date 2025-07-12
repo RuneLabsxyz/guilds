682c3f25638187d2117845d09ec740ade15998f7
Mugen <mugen@runelabs.xyz>
2025-07-12 16:58:51 +0200

    feat: flake setup

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Cairo toolchain in nix";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    let
+      overlay = import ./overlay.nix;
+    in
+    {
+      overlays = {
+        default = overlay;
+      };
+
+      templates = {
+        default = {
+          path = ./templates/simple;
+          description = "A basic project using cairo-nix";
+        };
+      };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cargo
+            cairo-bin.stable.cairo
+            cairo-bin.stable.scarb
+          ];
+        };
+
+        packages = {
+          default = pkgs.cairo-bin.stable.scarb;
+          cairo = pkgs.cairo-bin.stable.cairo;
+          scarb = pkgs.cairo-bin.stable.scarb;
+          cairo-beta = pkgs.cairo-bin.beta.cairo;
+          scarb-beta = pkgs.cairo-bin.beta.scarb;
+        };
+      });
+}
